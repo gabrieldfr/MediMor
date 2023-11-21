25b7f63ad8b291c037520eb8d654d0e09f49dabd
Gabriel Rocha <shadowninja2093@gmail.com>
2023-11-21 10:01:12 -0300

    pagina de login

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { } from 'react'
+import Nav from './components/Nav'
+import { Outlet } from 'react-router-dom'
+import Login from './routers/Login'
+
+function App() {
+
+  return (
+    <>
+    <Login />
+    
+    
+    </>
+  )
+}
+
+export default App
